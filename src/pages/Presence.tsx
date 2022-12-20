7740aed4037d73d9a194f7422ea1241a7dcab2a0
Adrian Blanc <adrian@adrianblanc.live>
2022-12-20 15:39:07 -0800

    sure

--- src/pages/Presence.tsx
+++ src/pages/Presence.tsx
@@ -8,14 +8,7 @@ const Presence = () => (
     </Helmet>
     <h1>👀 Presence</h1>
     <p>
-      You may have noticed that while I'm doing something like listening to Spotify, programming in
-      VSCode or playing a game, it'll appear in the bottom left of my site. This is thanks to an
-      open-source project I created called{' '}
-      <a target="_blank" rel="noreferrer" href={'https://github.com/phineas/lanyard'}>
-        Lanyard
-      </a>{' '}
-      which pulls live presences from Discord and updates an API and WebSocket service. It takes{' '}
-      {'<'}10 seconds to set up, you just have to join a Discord server!
+      I see who you are spying on me. :)
     </p>
   </PageWrapper>
 );
