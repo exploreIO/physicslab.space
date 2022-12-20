f77b2eae01f06934f146c1e62c5c841fea2e4be3
Adrian Blanc <adrian@adrianblanc.live>
2022-12-20 13:17:43 -0800

    proper wording 🔥

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,8 +19,7 @@ const Home = () => {
         y/o Engineer, Innovator and Inventor.
       </p>
       <p>
-        I love Engineering & Physics. I spend most of my time building Robots and ML algorithms while research in the feilds of Physics. Currently, I'm a Founder 
-        and Engineer at{' '}
+        I love Engineering & Physics. I spend most of my time building Robots and ML algorithms while research in the feilds of Physics. Currently, I run {' '}
         <a href="/" target="norel noopen">
           Nebulei
         </a>
@@ -31,12 +30,11 @@ const Home = () => {
         .
       </p>
       <p>
-        I specialize in Robotics & ML but eventually want to become an expert in physics, although far from there.
+        I specialize in Robotics & ML but eventually want to become proficient in physics, although far from there.
       </p>
 
       <p>
-        In my free time, I dont really do anything else off the internet but some of my hobbies besides work itself, is reading and 
-        chess basically. My end goal is too do space exploration.
+        In my free time, I like pondering and researching upon life's aspects.. My end goal is too do space exploration, among other things...
       </p>
 
     </PageWrapper>
